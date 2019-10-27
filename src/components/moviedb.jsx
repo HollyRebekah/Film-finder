4932a53844a75a6d2f7ce3a0506190cc0fdfb3a2
Holly Fanthorpe <hollyfanthorpe@gmail.com>
2019-10-27 15:49:29 +0000

    user account now renders movie image

--- src/components/moviedb.jsx
+++ src/components/moviedb.jsx
@@ -95,6 +95,7 @@ class Moviedb extends React.Component {
     Axios.post('http://localhost:8080/filmfinder/users/movie', {
       email: this.props.user.email,
       movie: this.state.currentMovie,
+      movieImage: this.state.image,
     });
     console.log(this.state.movieList);
     console.log(this.props.user.filmsWatched);
--- src/components/user-account.jsx
+++ src/components/user-account.jsx
@@ -5,9 +5,9 @@ const UserAccount = (props) => {
     <div className="user-info-page">
       <h1>User Info</h1>
       <div>Name: {props.user.firstName} {props.user.lastName}</div>
-      <div>Movies I've Watched:{props.user.filmsWatched.map((movie) => {
+      <div>Movies I've Watched:{props.user.filmImages.map((image) => {
         return (
-          <li key={props.user.filmsWatched.indexOf(movie)}>{movie}</li>
+          <li key={props.user.filmImages.indexOf(image)}><img src={image} /></li>
         );
       })}
       </div>
