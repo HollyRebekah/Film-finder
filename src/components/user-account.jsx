73df2aae71b694ea821ea77d6cc102b7154f8de7
Holly Fanthorpe <hollyfanthorpe@gmail.com>
2019-10-27 12:28:29 +0000

    added user details to the user info page and added icon that redirects to movie page

---
+++ src/components/user-account.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const UserAccount = (props) => {
+  return (
+    <div className="user-info-page">
+      <h1>User Info</h1>
+      <div>Name: {props.user.firstName} {props.user.lastName}</div>
+      <div>Movies I've Watached:{props.user.filmsWatched.map((movie) => {
+        return (
+          <li key={props.user.filmsWatched.indexOf(movie)}>{movie}</li>
+        );
+      })}
+      </div>
+    </div>
+  );
+};
+
+export default UserAccount;
