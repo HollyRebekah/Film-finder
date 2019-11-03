b65dd0075ddaf07f913768d5c46c986a0a9bdb36
Holly Fanthorpe <hollyfanthorpe@gmail.com>
2019-11-03 11:38:19 +0000

    user account pages now renders image from database

--- src/components/user-account.jsx
+++ src/components/user-account.jsx
@@ -1,19 +1,42 @@
 import React from 'react';
+import axios from 'axios';
 import '../styles/user-account.css';
 
-const UserAccount = (props) => {
-  return (
-    <div className="user-info-page">
-      <h1>User Info</h1>
-      <div>Name: {props.user.firstName} {props.user.lastName}</div>
-      <div>Movies I've Watched:{props.user.filmImages.map((image) => {
-        return (
-          <li className="movie-list" key={props.user.filmImages.indexOf(image)}><img className="movie" alt="movie" src={image} /></li>
-        );
-      })}
+class UserAccount extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: [],
+    };
+  }
+
+
+  componentDidMount() {
+    axios.post(
+      'http://localhost:8080/filmfinder/movies/image',
+      { title: this.props.user.filmsWatched }
+    ).then(response => {
+      console.log(response.data);
+      this.setState({
+        images: response.data,
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div className="user-info-page">
+        <h1>User Info</h1>
+        <div>Name: {this.props.user.firstName} {this.props.user.lastName}</div>
+        <div>Movies I've Watched:{this.state.images.map((image) => {
+          return (
+            <li className="movie-list" key={this.state.images.indexOf(image)}><img className="movie" alt="movie" src={image} /></li>
+          );
+        })}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default UserAccount;
